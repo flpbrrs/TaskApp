07f5c10a3936f9f534d7ae19e29cb88d20541396
Felipe Barros <felipejonathan.dev@gmail.com>
2025-03-17 22:25:07 -0300

    feat: criando navegação por tabs no home fragment via criação do view pager adapter

--- app/src/main/java/br/com/flpbrrs/taskapp/ui/HomeFragment.kt
+++ app/src/main/java/br/com/flpbrrs/taskapp/ui/HomeFragment.kt
@@ -3,10 +3,28 @@ package br.com.flpbrrs.taskapp.ui
 import android.os.Bundle
 import android.view.View
 import br.com.flpbrrs.taskapp.R
+import br.com.flpbrrs.taskapp.adapters.ViewPagerAdapter
 import br.com.flpbrrs.taskapp.databinding.FragmentHomeBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeFragment : GenericFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initTabs()
+    }
+
+    private fun initTabs() {
+        val pagerAdapter = ViewPagerAdapter(requireActivity())
+        binding.viewPager.adapter = pagerAdapter
+
+        pagerAdapter.addFragment(TodoFragment(), R.string.tab_title_todo)
+        pagerAdapter.addFragment(DoingFragment(), R.string.tab_title_doing)
+        pagerAdapter.addFragment(DoneFragment(), R.string.tab_title_done)
+
+        binding.viewPager.offscreenPageLimit = pagerAdapter.itemCount
+
+        TabLayoutMediator(binding.tabs, binding.viewPager) { tab, position ->
+            tab.text = getString(pagerAdapter.getTitle(position))
+        }.attach()
     }
 }
