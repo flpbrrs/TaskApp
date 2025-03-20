c3e83d918eacc2d3f2467a344198dd91334b0971
Felipe Barros <felipejonathan.dev@gmail.com>
2025-03-20 19:03:25 -0300

    feat: criação do formulário de nova task e adicionando drawables de estilização

---
+++ app/src/main/java/br/com/flpbrrs/taskapp/ui/FormTaskFragment.kt
@@ -0,0 +1,8 @@
+package br.com.flpbrrs.taskapp.ui
+
+import br.com.flpbrrs.taskapp.R
+import br.com.flpbrrs.taskapp.databinding.FragmentFormTaskBinding
+
+class FormTaskFragment : GenericFragment<FragmentFormTaskBinding>(FragmentFormTaskBinding::inflate) {
+
+}
