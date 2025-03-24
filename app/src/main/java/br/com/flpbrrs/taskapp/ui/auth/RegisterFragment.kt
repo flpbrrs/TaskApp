4d309da709e531a8cf689901666dd7d95771d4b6
Felipe Barros <felipejonathan.dev@gmail.com>
2025-03-24 17:04:45 -0300

    feat: adição da barra de navegação na tela de registro

--- app/src/main/java/br/com/flpbrrs/taskapp/ui/auth/RegisterFragment.kt
+++ app/src/main/java/br/com/flpbrrs/taskapp/ui/auth/RegisterFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.View
 import br.com.flpbrrs.taskapp.databinding.FragmentRegisterBinding
 import br.com.flpbrrs.taskapp.ui.GenericFragment
+import br.com.flpbrrs.taskapp.utils.initToolbar
 
 class RegisterFragment : GenericFragment<FragmentRegisterBinding>(FragmentRegisterBinding::inflate) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initToolbar(binding.fragmentToolbar)
     }
 }
--- app/src/main/java/br/com/flpbrrs/taskapp/utils/FragmentExtensions.kt
+++ app/src/main/java/br/com/flpbrrs/taskapp/utils/FragmentExtensions.kt
@@ -1,8 +1,17 @@
 package br.com.flpbrrs.taskapp.utils
 
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import br.com.flpbrrs.taskapp.components.BottomSheetContainerFragment
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+fun Fragment.initToolbar(toolbar: Toolbar) {
+    (activity as AppCompatActivity).setSupportActionBar(toolbar)
+    (activity as AppCompatActivity).title = ""
+    (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    toolbar.setNavigationOnClickListener { activity?.onBackPressedDispatcher?.onBackPressed() }
+}
 
 fun Fragment.showBottomSheetContainerFor(fragment: Fragment) {
     BottomSheetContainerFragment(fragment)
