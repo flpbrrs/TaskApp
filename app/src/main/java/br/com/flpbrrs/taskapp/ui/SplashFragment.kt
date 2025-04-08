696b4109b1138ff53a4203645ee356dd51ea89bc
Felipe Barros <felipejonathan.dev@gmail.com>
2025-04-08 17:29:17 -0300

    refactor: update da lógica do SplashFragment

--- app/src/main/java/br/com/flpbrrs/taskapp/ui/SplashFragment.kt
+++ app/src/main/java/br/com/flpbrrs/taskapp/ui/SplashFragment.kt
@@ -7,31 +7,22 @@ import androidx.navigation.fragment.findNavController
 import br.com.flpbrrs.taskapp.R
 import br.com.flpbrrs.taskapp.components.GenericFragment
 import br.com.flpbrrs.taskapp.databinding.FragmentSplashBinding
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import br.com.flpbrrs.taskapp.utils.FirebaseHelper
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SplashFragment : GenericFragment<FragmentSplashBinding>(FragmentSplashBinding::inflate) {
-
-    private lateinit var auth: FirebaseAuth
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        auth = Firebase.auth
         viewLifecycleOwner.lifecycleScope.launch {
-            delay(2000)
-            checkAuth()
-        }
-    }
-
-    private fun checkAuth() {
-        val currentUser = auth.currentUser
-        if (currentUser != null) {
-            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
-        } else {
-            findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+            delay(1000)
+            findNavController().navigate(
+                if (FirebaseHelper.isAuthenticated()) {
+                    R.id.action_splashFragment_to_homeFragment
+                } else {
+                    R.id.action_splashFragment_to_loginFragment
+                }
+            )
         }
     }
 }
